81e403f4909981c4b6ed8dbc22c561bafffb57b9
princeampofo <pla252@nyu.edu>
2022-05-27 19:45:00 +0400

    minor changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { registerLicense } from '@syncfusion/ej2-base';
 
 
-registerLicense('@32302e312e30ajVnVVj/1Ne0PM7554z6wVswry765LCY+VM71hgJ8Ww=');
+registerLicense('ORg4AjUWIQA/Gnt2VVhhQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkBhXn5fc31UT2VZU0c=');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
