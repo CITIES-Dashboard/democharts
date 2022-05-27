1f1d455e82eab83666156ccdea164e1fde2db921
princeampofo <pla252@nyu.edu>
2022-05-27 14:23:43 +0400

    minor changes

--- src/data.js
+++
@@ -1,8 +0,0 @@
-exports.salesData = [
-    {month:'Jan',sales:35},{month:'Feb',sales:28},
-    {month:'Mar',sales:38},{month:'Apr',sales:47},
-    {month:'May',sales:20},{month:'Jun',sales:45},
-    {month:'Jul',sales:12},{month:'Aug',sales:28},
-    {month:'Sep',sales:43},{month:'Oct',sales:48},
-    {month:'Nov',sales:55},{month:'Dec',sales:28}
-]
