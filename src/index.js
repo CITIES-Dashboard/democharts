e09214a5f09a8dc3f5c72a32c9be4b3ac4262334
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2022-06-14 15:13:57 +0700

    fix chart not showing with data from google sheet

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import demo from './demo';
 import { registerLicense } from '@syncfusion/ej2-base';
 
+import {fetchSheetData} from './data.js';
 
-
+// SyncFusion license key
 registerLicense('ORg4AjUWIQA/Gnt2VVhhQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkBhXn5fc31UT2VZU0c=');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-    <demo />
-  </React.StrictMode>
-);
+
+const google = window.google;
+google.charts.load('current');
+google.charts.setOnLoadCallback(fetchSheetData);
+
+export function renderApp(){
+  console.log("Calling root.render")
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+
 
 
